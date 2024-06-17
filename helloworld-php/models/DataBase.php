24151b21fa967e512f8937ab82118ccad55b92a2
Jose Bohorquez <bd567358546@gmail.com>
2024-06-17 02:09:41 -0500

    ajustando docker file y mensaje de conexion

--- helloworld-php/models/DataBase.php
+++ helloworld-php/models/DataBase.php
@@ -1,15 +1,23 @@
 <?php
-    class DataBase{
-        public static function connection(){
-            $hostname = getenv('DB_HOST'); // Suponiendo que la variable de entorno se llama DB_HOST
-            $port = getenv('DB_PORT');
-            $database = getenv('DB_DATABASE');
-            $username = getenv('DB_USERNAME');
-            $password = getenv('DB_PASSWORD');
+class DataBase {
+    public static function connection() {
+        $hostname = getenv('DB_HOST'); // Suponiendo que la variable de entorno se llama DB_HOST
+        $port = getenv('DB_PORT');
+        $database = getenv('DB_DATABASE');
+        $username = getenv('DB_USERNAME');
+        $password = getenv('DB_PASSWORD');
 
-            $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+        try {
+            $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            // Si la conexión se establece correctamente, retornamos el objeto PDO
+            echo "Conexión exitosa a la base de datos.";
             return $pdo;
+        } catch(PDOException $e) {
+            // Si ocurre un error al conectar, capturamos la excepción y la imprimimos
+            die("Error al conectar a la base de datos: " . $e->getMessage());
         }
     }
+}
 ?>
